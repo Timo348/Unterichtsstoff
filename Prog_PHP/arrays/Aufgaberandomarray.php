5e2281ba78461d95103ace7eef72df9c7354d2c5
Timohub3005 <Timohoppe07@gmail.com>
2024-11-27 12:19:44 +0100

    .

--- Prog_PHP/arrays/Aufgaberandomarray.php
+++ Prog_PHP/arrays/Aufgaberandomarray.php
@@ -7,9 +7,11 @@
 </head>
 <body>
     <?php
-    /* Array Erstellen 20 Zufälligen Zahlen mit rand()
+    /* Array Erstellen 20 Zufälligen Zahlen mit rand(), Zahlen sollen zwichen 1 und 100 sein
     Berechne die Summe, durschnitt, Median(der mittlere wert des Arrays), Quadratischer Mittelwert,
     Quadratischer Mittelwert), Anzahl der Geraden und ungeraden */
+
+    // Übung Array PHP
 
     ?>
 </body>
