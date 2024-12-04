ce10b13cc446b2389951758d65f9c08b147a9af3
Timohub3005 <Timohoppe07@gmail.com>
2024-12-04 12:22:27 +0100

    .

--- Prog_PHP/arrays/gamble/eDicer.php
+++ Prog_PHP/arrays/gamble/eDicer.php
@@ -3,22 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>eDicer</title>
+    <title>E-Dicer</title>
 </head>
 <body>
-<form action="">
-    <input id="anzahl" name="anzahl" type="number" min="1" required>
-    <input type="submit" value="Roll Dice">
-</form>
+<form action="edicer.php">
+        <input type="number" min="1" name="anzahl_wuerfel" placeholder="Anzahl Würfe"><br>
+        <button type="submit">GAMBLE</button>
+    </form>
+    <div class="board">
+    <?php 
+    $augen_array = [];
 
-<?php
-if (isset($_REQUEST['anzahl'])) {
-    $anzahl = $_REQUEST['anzahl'];
-    for ($i = 0; $i < $anzahl; $i++) {
-        $augen = rand(1, 6);
-        echo "<img width='100' height='100' src='$augen.png' alt='$augen Augen'>";
+    $augen = rand(1,6);
+    $anzahl = $_REQUEST['anzahl_wuerfel'];
+    
+    // Array füllen
+    for ($i=1; $i<=$anzahl; $i++){
+        $augen = rand(1,6);
+        $augen_array[] = $augen;
+        //echo "<img width = '10%' src = '/bilder/$augen.png' alt='$augen Augen'>\n";
     }
-}
-?>
+    // Array ausgeben
+    /*echo "<pre>";
+    print_r($augen_array);
+    echo"</pre>";
+    */
+    sort($augen_array);
+    for ($i=0; $i<$anzahl; $i++){
+        echo "<img width = '10%' src = '$augen_array[$i].png' alt='$augen_array[$i] Augen'>\n";
+    }
+    $summe = array_sum($augen_array);
+    echo "<div class='ausgabe'>Die Summe der Augenzahlen beträgt $summe</div>";
+    echo "</div>"
+    ?>
 </body>
 </html>
